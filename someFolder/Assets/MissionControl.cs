e27b8de10befcf18e55a1c5b4b3df363542d21f5
norrox <christoffer_karlsson@live.se>
2017-11-25 00:27:43 +0100

    Mission Control!

---
+++ someFolder/Assets/MissionControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionControl : MonoBehaviour {
+
+	public Text mission1Text;
+	public Text mission2Text;
+	public Text mission3Text;
+
+	public Button acceptMission1;
+	public Button acceptMission2;
+	public Button acceptMission3;
+
+	// Use this for initialization
+	void Start () {
+		acceptMission1.onClick.AddListener(GoToM1);
+		acceptMission2.onClick.AddListener(GoToM2);
+		acceptMission3.onClick.AddListener(GoToM3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void GoToM1() {
+		//Goto Mission 1
+		print("Going to mission 1");
+	}
+
+	void GoToM2() {
+		//Goto Mission 2
+		print("Going to mission 2");
+	}
+
+	void GoToM3() {
+		//Goto Mission 3
+		print("Going to mission 3");
+	}
+}
---
+++ someFolder/Assets/Scripts/scrollText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scrollText : MonoBehaviour {
+	
+	private Text text;
+	public float textScrollSpeed = 50f;
+	private string moveDir = "right";
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if(moveDir == "right")
+			text.transform.position += new Vector3(1, 0f, 0f) * textScrollSpeed * Time.deltaTime;
+		else
+			text.transform.position -= new Vector3(1, 0f, 0f) * textScrollSpeed * Time.deltaTime;
+
+		if(text.transform.localPosition.x > -220f) {
+			moveDir = "right";
+			print("move Right");
+		}
+		else if(text.transform.localPosition.x < 220f){
+			moveDir = "left";
+			print("move left");
+		}
+*/
+		
+	}
+}
